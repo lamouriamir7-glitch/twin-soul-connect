edd0590a90dbb40ad2a7defb3f132ae273303626
lamouriamir7-glitch <lamouriamir7@gmail.com>
2026-05-05 04:39:12 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.tsx";
 import "./index.css";
 import { LanguageProvider } from "./i18n/LanguageContext";
 
 createRoot(document.getElementById("root")!).render(
-  <Auth0Provider
-    domain="dev-ii30ojv5xtugbd1u.eu.auth0.com"
-    clientId="BhO9vrG9BU9kwvH0BeWnECT5CbSXpXcy"
-    cacheLocation="localstorage"
-    useRefreshTokens={true}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
-  </Auth0Provider>
+  <LanguageProvider>
+    <App />
+  </LanguageProvider>
 );
